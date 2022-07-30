c4a62388796eaf63743d33563d73cfde3fc6221a
Titouan Chevrier <chevrier.titouan@gmail.com>
2022-07-30 23:27:37 +0200

    test

---
+++ Libft/srcs/libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h		                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tchevrie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/01 18:38:45 by tchevrie          #+#    #+#             */
+/*   Updated: 2022/08/01 13:05:34 by tchevrie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <stdio.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s);
+
+#endif
