860a0b339218530a5634ba6e3d3b4a0a7ca65ff4
titouan_ck <chevrier.titouan@gmail.com>
2022-09-10 18:24:19 +0200

    Libft Sat 10 Sept at 18:24

--- Libft/includes/libft.h
+++ Libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: tchevrie <tchevrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 15:13:35 by tchevrie          #+#    #+#             */
-/*   Updated: 2022/09/09 17:15:08 by tchevrie         ###   ########.fr       */
+/*   Updated: 2022/09/10 18:22:06 by tchevrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,12 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
+char	*ft_strcat(char *restrict s1, const char *restrict s2);
+char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
+size_t	ft_strlcat(char *restrict d, const char *restrict s, size_t dstsize);
+char	*ft_strchr(const char *s, int c);
 
 #endif
